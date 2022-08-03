03a2bb5dc62b2da6e576955297c28dd5052924c2
Michael Schönburg <82150528+MichaelSchoenburg@users.noreply.github.com>
2022-08-03 13:27:53 +0200

    Initial commit

---
+++ Template.ps1
@@ -0,0 +1,113 @@
+<#
+.SYNOPSIS
+    T22-000215 - Rechnungsversand automatisieren
+
+.DESCRIPTION
+    Long description
+
+.SYNTAX
+
+
+.PARAMETERS
+
+
+.EXAMPLE
+    PS C:\> <example usage>
+    Explanation of what the example does
+
+.INPUTS
+    Inputs (if any)
+
+.OUTPUTS
+    Output (if any)
+
+.RELATED LINKS
+    GitHub: https://github.com/MichaelSchoenburg/T22-000215
+
+.NOTES
+    Author: Michael Schönburg
+    Version: v1.0
+    Last Edit: 11.02.2021
+    
+    This projects code loosely follows the PowerShell Practice and Style guide, as well as Microsofts PowerShell scripting performance considerations.
+    Style guide: https://poshcode.gitbook.io/powershell-practice-and-style/
+    Performance Considerations: https://docs.microsoft.com/en-us/powershell/scripting/dev-cross-plat/performance/script-authoring-considerations?view=powershell-7.1
+
+.REMARKS
+    To see the examples, type: "get-help Get-HotFix -examples".
+    For more information, type: "get-help Get-HotFix -detailed".
+    For technical information, type: "get-help Get-HotFix -full".
+    For online help, type: "get-help Get-HotFix -online"
+#>
+
+#region INITIALIZATION
+<# 
+    Libraries, Modules, ...
+#>
+
+#endregion INITIALIZATION
+#region DECLARATIONS
+<#
+    Declare local variables and global variables
+#>
+
+
+#endregion DECLARATIONS
+#region FUNCTIONS
+<# 
+    Declare Functions
+#>
+
+function Write-ConsoleLog {
+    <#
+    .SYNOPSIS
+    Logs an event to the console.
+    
+    .DESCRIPTION
+    Writes text to the console with the current date (US format) in front of it.
+    
+    .PARAMETER Text
+    Event/text to be outputted to the console.
+    
+    .EXAMPLE
+    Write-ConsoleLog -Text 'Subscript XYZ called.'
+    
+    Long form
+    .EXAMPLE
+    Log 'Subscript XYZ called.
+    
+    Short form
+    #>
+    [alias('Log')]
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true,
+        Position = 0)]
+        [string]
+        $Text
+    )
+
+    # Save current VerbosePreference
+    $VerbosePreferenceBefore = $VerbosePreference
+
+    # Enable verbose output
+    $VerbosePreference = 'Continue'
+
+    # Write verbose output
+    Write-Verbose "$( Get-Date -Format 'MM/dd/yyyy HH:mm:ss' ) - $( $Text )"
+
+    # Restore current VerbosePreference
+    $VerbosePreference = $VerbosePreferenceBefore
+}
+
+
+
+#endregion FUNCTIONS
+#region EXECUTION
+<# 
+    Script entry point
+#>
+
+
+
+#endregion EXECUTION
